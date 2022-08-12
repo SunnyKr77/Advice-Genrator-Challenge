89f7f7fb829340255b02492b5a071eaff05763b3
Sunny Shah <sunnykrshah201@gmail.com>
2022-08-12 14:49:17 +0530

    added try and catch

--- scripts/script.js
+++ scripts/script.js
@@ -2,37 +2,24 @@ const advise = document.getElementById('card__advice')
 const adviseNum = document.querySelector('#card__num')
 const adviseBtn = document.getElementById('adviseBtn')
 
+
 adviseBtn.addEventListener('click', generateAdvise)
 
 async function generateAdvise() {
-    const result = await fetch('https://api.adviceslip.com/advice')
-    .then((response) => response.json())
-    .then((data) => data.slip);
-
-    adviseNum.innerHTML = `<span>${result.id}</span>`;
-    advise.innerHTML = `<div>${result.advice}</div>`;
+    try {
+        const result = await fetch('https://api.adviceslip.com/advice')
+        .then((response) => response.json())
+        .then((data) => data.slip);
+    
+        // adviseNum.innerHTML = `<span>${result.id}</span>`;
+        adviseNum.innerHTML = `<span>${result.idsk}</span>`;
+        advise.innerHTML = `<div>${result.advice}</div>`;
+    } catch (error) {
+        throw new Error (`Something went wrong`)
+    }
 }
 generateAdvise()
 
-// console.log(generateAdvise);
 
-// const adviseFunction = (value) => {
-//     console.log(value);
-// }
-
-// const advice = document.getElementById('advice')
-// const adviceId = document.querySelector('#card__id')
-// const btn = document.getElementById('button')
-
-// btn.addEventListener('click', generateAdvise)
-
-// async function generateAdvise()  {
-//     const result = await fetch('https://api.adviceslip.com/advice').then((response) => response.json()).then((data) => data.slip);
-
-//     console.log(result);    
-
-//     adviceId.innerHTML = result.id
-//     advice.innerHTML = result.advice
-// }
 
 
